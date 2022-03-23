b62060708a53aee0eabf2ac4e72954372cf56146
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2022-03-23 14:09:15 +0300

    Update INSTALL_RUN.bat

--- INSTALL_RUN.bat
+++ INSTALL_RUN.bat
@@ -1,6 +1,10 @@
 @Echo off
 title LTCALL MMDRZA.COM
 Pushd "%~dp0"
+pip install hdwallet
+pip install colorama
+pip install lxml
+pip install requests
 start P2PKH.bat
 start P2SWH.bat
 start P2WPKH.bat
